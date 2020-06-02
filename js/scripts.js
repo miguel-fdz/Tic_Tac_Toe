6c9bc48b88fff026d7db8977ac79ab929164f046
Miguel Fernandez <miguelf.olalla@gmail.com>
2020-06-02 17:42:47 -0300

    Added round win logic and started working on page aesthetics

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,8 @@
 const addPlayer = (name) => {
   let symbol;
   (gameboard.players.length > 0) ? symbol = 'O' : symbol = 'X';
-  const toggleTurn = () => {};
-  const playerWin = () => {};
-  return {name, symbol};
+  const playerWin = () => {alert("Round over!")};
+  return {name, symbol, playerWin};
 };
 
 
@@ -13,6 +12,9 @@ let gameboard = {
     board: ['','','','','','','','',''],
     players: [],
     turn: 0,
+    toggleTurn: () => {
+
+    },
     render: () => {
       const board = document.getElementById("gameboard");
       while (board.firstChild) {
@@ -24,10 +26,7 @@ let gameboard = {
         newTile.setAttribute("data-index", `${i}`);
         newTile.innerHTML = gameboard.board[i];
         board.appendChild(newTile);
-        newTile.addEventListener("click", (e) => {
-          gameModule.makeMove(e.target.dataset.index);
-          gameboard.render();
-        })
+        newTile.addEventListener("click", (e) => gameModule.takeTurn(e.target.dataset.index))
       }
     }
   }
@@ -38,7 +37,7 @@ const gameModule = (() => {
 
   /** Game settings selection */
   let settings = {
-    mode: ''
+    mode: '',
   }
 
   document.querySelectorAll(".settings__button").forEach(button => {
@@ -46,9 +45,8 @@ const gameModule = (() => {
     let playerOneName = document.getElementById("playerOneName");
     let playerTwoName = document.getElementById("playerTwoName");
     button.addEventListener("click", (e) => {
-      //insert newGame button functionality
       if (e.target.id === "newGame") {
-        document.getElementById("settingsModal").classList.toggle("modal")
+        document.getElementById("modalOverlay").classList.toggle("closed")
       };
       if (e.target.classList.contains("mode__button")) {
         gameModule.settings.mode = e.target.id;
@@ -62,20 +60,50 @@ const gameModule = (() => {
         } else if (gameModule.settings.mode === "PvAI") {
         //If PvAI selection, allow input of only one player
         }
-        document.getElementById("gameboard").classList.toggle("gameboard")
+        document.getElementById("modalOverlay").classList.toggle("closed")
+        document.getElementById("gameboard").classList.toggle("closed")
       }
     })
   })
 
   /** Gameplay logic */
-  function makeMove(tile) {
+  function takeTurn(tile) {
+    const symbol = gameboard.players[gameboard.turn].symbol;
     if (gameboard.board[tile] === '') {
-      gameboard.board[tile] = gameboard.players[gameboard.turn].symbol;
-      (gameboard.turn == 0) ? gameboard.turn = 1 : gameboard.turn = 0;
+      gameboard.board[tile] = symbol;
+      gameboard.render();
+      gameModule.checkWin(symbol);
+      gameModule.toggleTurn();
     }
   }
 
-  return {settings, makeMove}
+  function toggleTurn() {
+    (gameboard.turn == 0) ? gameboard.turn = 1 : gameboard.turn = 0;
+  }
+
+  function checkWin(symbol) {
+    const winArrays = {
+      'Top3':   [0,1,2],
+      'HMid3':  [3,4,5],
+      'Low3':   [6,7,8],
+      'Left3':  [0,3,6],
+      'VMid3':  [1,4,7],
+      'Right3': [2,5,8],
+      'LDiag3': [0,4,8],
+      'RDiag3': [2,4,6]
+    }
+    /** map over array to turn tiles with matching symbol into their respective 
+    index value, else turning into falsy value, then filter out falsy values */
+    const tilesPlayed = gameboard.board.map((tile,index) => (tile === symbol) ? index : undefined)
+                                       .filter(x => (x || x === 0)); //don't filter out '0' as falsy
+    Object.values(winArrays).forEach(subArray => {
+      if (subArray.every(e => tilesPlayed.includes(e))) {
+        return gameboard.players[gameboard.turn].playerWin();
+      }
+    })
+  }
+
+  return {settings, takeTurn, toggleTurn, checkWin}
 })();
 
 
